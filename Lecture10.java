4ca8cb7efed962913482db0cf099789f5adc4d2d
Shah Aatif Bashir <95216767+ShahAatif@users.noreply.github.com>
2024-01-30 19:41:09 +0530

    Static Keyword Usage

---
+++ Lecture10.java
@@ -0,0 +1,27 @@
+// this keyword
+// static keyword and its uses
+public class Lecture10 {
+    int rollno;
+    String name;
+    static int count = 0;
+
+    Lecture10(int rollno, String namString) {
+        this.rollno = rollno;
+        this.name = name;
+        count++;
+    }
+
+    static void show() {
+        System.out.println(count);
+    }
+
+    public static void main(String[] args) {
+        Lecture10 a = new Lecture10(1, "danish");
+        System.out.println(count);
+        Lecture10 b = new Lecture10(1, "danish");
+        System.out.println(count);
+        Lecture10 c = new Lecture10(1, "danish");
+        System.out.println(Lecture10.count);
+        Lecture10.show();
+    }
+}
